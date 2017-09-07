54088b93ecf79e12950b353127d4f24ff11ebd62
codyhoag <cody.hoag@liferay.com>
2017-09-07 11:49:02 -0400

    Insert 01-HelloUserPortlet code snippet

--- liferay-workspace/modules/hello-user/src/main/java/hello/user/portlet/HelloUserPortlet.java
+++ liferay-workspace/modules/hello-user/src/main/java/hello/user/portlet/HelloUserPortlet.java
@@ -3,20 +3,24 @@ package hello.user.portlet;
 import hello.user.constants.HelloUserPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 
-/**
- * @author cdhoag
- */
 @Component(
 	immediate = true,
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=hello-user Portlet",
+		"javax.portlet.display-name=Hello User Portlet",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + HelloUserPortletKeys.HelloUser,
@@ -26,4 +30,21 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class HelloUserPortlet extends MVCPortlet {
+
+	@Override
+	public void processAction(ActionRequest actionRequest, ActionResponse actionResponse)
+			throws IOException, PortletException {
+
+		PortletPreferences prefs = actionRequest.getPreferences();
+		String userName = actionRequest.getParameter("userName");
+
+		if (userName != null) {
+			prefs.setValue("userName", userName);
+			prefs.store();
+			SessionMessages.add(actionRequest, "request_processed",
+				"Your name saved successfully! Click 'Back' to view it.");
+		}
+
+		super.processAction(actionRequest, actionResponse);
+	}
 }
